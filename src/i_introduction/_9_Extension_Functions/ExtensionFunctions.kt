42a4898166d9a96d4ec29c4738d9890cc79ca925
Johnny Wey <johnnywey@gmail.com>
2017-04-10 16:22:10 -0600

    Koaning

--- src/i_introduction/_9_Extension_Functions/ExtensionFunctions.kt
+++ src/i_introduction/_9_Extension_Functions/ExtensionFunctions.kt
@@ -3,7 +3,7 @@ package i_introduction._9_Extension_Functions
 import util.TODO
 import util.doc9
 
-fun String.lastChar() = this.get(this.length - 1)
+fun String.lastChar() = this[this.length - 1]
 
 // 'this' can be omitted
 fun String.lastChar1() = get(length - 1)
@@ -27,7 +27,7 @@ fun todoTask9(): Nothing = TODO(
 
 data class RationalNumber(val numerator: Int, val denominator: Int)
 
-fun Int.r(): RationalNumber = todoTask9()
-fun Pair<Int, Int>.r(): RationalNumber = todoTask9()
+fun Int.r(): RationalNumber = RationalNumber(this, 1)
+fun Pair<Int, Int>.r(): RationalNumber = RationalNumber(this.first, this.second)
 
 
